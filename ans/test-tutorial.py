91586f6f00373507a0b5001fc4696bbbf8d31da3
AnsKat <ans.pardons@gmail.com>
2016-02-27 11:36:01 +0100

    My Django Girls app, first commit

---
+++ ans/test-tutorial.py
@@ -0,0 +1,12 @@
+def hi(name):
+      print("The wonderful "+ name + "!")
+
+everybody=["Judy", "Deborah","Jane", "A", "B", "C"]
+print("Here we go...")
+for name in everybody:
+  hi(name)
+  print("and the next is:")
+print(" Ans")
+
+for i in range(0,6):
+  print(everybody[i] + " is in the house")
